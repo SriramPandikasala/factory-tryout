21ec28610a34ba8e4d7042acb934dc849198a757
SriramPandikasala <psriramajayam@gmail.com>
2024-04-21 19:42:08 +0530

    [App] Added home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { HomeComponent } from './module/HomeComponent/HomeComponent'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-    This is React
+       <HomeComponent></HomeComponent>
       </>
   )
 }
